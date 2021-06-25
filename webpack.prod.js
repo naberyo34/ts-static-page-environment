bae5dc0e4f646de6a3bad89c245edd776faa0a5d
ryo.watanabe <naberyo34@gmail.com>
2021-06-25 14:39:56 +0900

    fix: webpack

--- webpack.prod.js
+++ webpack.prod.js
@@ -24,7 +24,15 @@ module.exports = {
       },
       {
         test: /\.ts$/,
-        use: 'ts-loader',
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env', '@babel/preset-typescript'],
+            },
+          },
+        ],
       },
     ],
   },
